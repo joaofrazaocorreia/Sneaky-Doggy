1bb75355816e6f456c95b8383624353ddba25da3
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2024-05-08 10:12:54 +0100

    Add playDead mechanic and de-aggro timer to enemies

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public float moveSpeed = 4f;
+    public float aggroTime = 2f;
+    public GameObject[] movementTargets;
     public PlayerMovement player;
 
     private Rigidbody2D rb;
     private Vector2 movement;
-    private bool sawPlayer;
+    private float aggroTimer;
+    private Vector3 startPos;
+    private GameObject currentTarget;
     
 
     // Start is called before the first frame update
@@ -17,21 +21,46 @@ public class EnemyMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         movement = new Vector2();
-        sawPlayer = false;
+        aggroTimer = 0f;
+        startPos = transform.position;
+        //currentTarget = movementTargets[0];
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 toPlayer = player.transform.position - transform.position;
+        Vector2 toStart = startPos - transform.position;
 
-        if (toPlayer.magnitude <= 3f)
-            sawPlayer = true;
+        if (toPlayer.magnitude <= 3f && !player.playingDead)
+        {
+            aggroTimer = aggroTime;
+        }
 
 
-        if (sawPlayer && toPlayer.magnitude > 0.1f)
+        if (aggroTimer > 0 && toPlayer.magnitude > 0.1f && !player.playingDead)
         {
             movement = toPlayer;
+            movement.Normalize();
+            rb.velocity = movement * moveSpeed;
+
+            if (movement != Vector2.zero)
+            {
+                Quaternion rotation = Quaternion.LookRotation(Vector3.forward, movement);
+                transform.rotation = rotation;
+            }
+        }
+
+        else if (aggroTimer > 0 && player.playingDead)
+        {
+            rb.velocity = Vector2.zero;
+            aggroTimer -= Time.deltaTime;
+        }
+
+        else if (toStart.magnitude > 0.1f && aggroTimer <= 0)
+        {
+            movement = toStart;
+            
             movement.Normalize();
             rb.velocity = movement * moveSpeed;
 
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,38 +5,61 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 4f;
+    public Sprite defaultSprite;
+    public Sprite playDeadSprite;
 
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
     private Vector2 moveInput;
     private bool hasObjective;
+    [HideInInspector] public bool playingDead;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponentInChildren<SpriteRenderer>();
         moveInput = new Vector2();
         hasObjective = false;
+        playingDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
-
-        moveInput.Normalize();
-        rb.velocity = moveInput * moveSpeed;
-
-        if (moveInput != Vector2.zero)
+        if (Input.GetKey(KeyCode.Q))
         {
-            Quaternion rotation = Quaternion.LookRotation(Vector3.forward, moveInput);
-            transform.rotation = rotation;
+            playingDead = true;
+            rb.velocity = Vector2.zero;
+            sr.sprite = playDeadSprite;
+        }
+
+        else
+        {
+            playingDead = false;
+            sr.sprite = defaultSprite;
+        }
+        
+
+        if (!playingDead)
+        {
+            moveInput.x = Input.GetAxisRaw("Horizontal");
+            moveInput.y = Input.GetAxisRaw("Vertical");
+
+            moveInput.Normalize();
+            rb.velocity = moveInput * moveSpeed;
+
+            if (moveInput != Vector2.zero)
+            {
+                Quaternion rotation = Quaternion.LookRotation(Vector3.forward, moveInput);
+                transform.rotation = rotation;
+            }
         }
     }
         
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.transform.tag == "Enemy")
+        if(collision.transform.tag == "Enemy" && !playingDead)
         {
             gameObject.SetActive(false);
             Debug.Log("You lose");
@@ -52,6 +75,7 @@ public class PlayerMovement : MonoBehaviour
         if(collision.transform.tag == "Entrance" && hasObjective)
         {
             Debug.Log("You win");
+            hasObjective = false;
         }
     }  
 }
